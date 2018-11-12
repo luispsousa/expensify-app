df220eaeae384498c91871280837868854dbccfc
Luis Sousa <luispsousa@ua.pt>
2018-11-12 18:19:14 +0000

    React-Redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -12,7 +12,7 @@ const decrementCount = ({ decrementBy = 1 } = {}) => ({
   decrementBy: decrementBy
 });
 
-const setCount = ({ count }) => ({
+const setCount = (count) => ({
   type: 'SET',
   count
 });
@@ -21,9 +21,12 @@ const resetCount = () => ({
   type: 'RESET',
 });
 
+// Reducers
+// 1. Reducers are pure function
+// 2. Never change state or action
 
-const store = createStore((state = { count: 0 }, action) => {
-  switch(action.type) {
+const countReducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
     case 'INCREMENT':
       return {
         count: state.count + action.incrementBy
@@ -35,15 +38,18 @@ const store = createStore((state = { count: 0 }, action) => {
     case 'SET':
       return {
         count: action.count
-      }
-    case 'RESET': 
+      };
+    case 'RESET':
       return {
         count: 0
       };
     default:
       return state;
   }
-});
+}
+
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
@@ -68,4 +74,4 @@ store.dispatch(decrementCount());
 
 store.dispatch(decrementCount({ decrementBy: 10 }));
 
-store.dispatch(setCount());
+store.dispatch(setCount(11));
